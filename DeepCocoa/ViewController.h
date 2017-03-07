3d00cafd56fed68bc36a900bf10fb140a90748f1
chenminhe <chenminhe@51talk.com>
2017-03-07 18:25:08 +0800

    Initial Commit

---
+++ DeepCocoa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepCocoa
+//
+//  Created by AC-Mac on 3/7/17.
+//  Copyright © 2017 MackChan  minhechen@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeepCocoa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DeepCocoa
+//
+//  Created by AC-Mac on 3/7/17.
+//  Copyright © 2017 MackChan  minhechen@gmail.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
